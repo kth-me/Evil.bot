21c8110f8b21b77852ce3b93e2e528e51977e669
Benjamin Hook <benjamin.hook@outlook.com>
2019-11-17 17:24:18 +1000

    Add Utilities class and alerts json

---
+++ kBot/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace kBot
+{
+    class Utilities
+    {
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+        }
+    }
+}
