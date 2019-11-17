b9748b399e667cfb909d444d24ff8a25b39e6f09
Benjamin Hook <benjamin.hook@outlook.com>
2019-11-17 22:19:43 +1000

    Add Discord functionality

---
+++ kBot/Config.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace kBot
+{
+    class Config
+    {
+        private const string configFolder = "Resources";
+        private const string configFile = "config.json";
+
+        public static BotConfig bot;
+
+        static Config()
+        {
+            if (!Directory.Exists(configFolder))
+            {
+                Directory.CreateDirectory(configFolder);
+            }
+
+            if (!File.Exists(configFolder + "/" + configFile))
+            {
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(configFolder + "/" + configFile, json);
+            }
+            else
+            {
+                string json = File.ReadAllText(configFolder + "/" + configFile);
+                bot = JsonConvert.DeserializeObject<BotConfig>(json);
+            }
+        }
+    }
+
+    public struct BotConfig
+    {
+        public string token;
+        public string cmdPrefix;
+    }
+}
