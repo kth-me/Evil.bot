c44046627638a562c51a54889b4c4ee9548db6fe
Benjamin Hook <benjamin.hook@outlook.com>
2019-11-17 14:08:53 +1000

    Update Program.cs

--- kBot/Program.cs
+++ kBot/Program.cs
@@ -6,7 +6,7 @@ namespace kBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
