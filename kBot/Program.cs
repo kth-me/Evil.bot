0a920ff7f6efdbfd590559c51a46abe0f0fd7e96
Benjamin Hook <benjamin.hook@outlook.com>
2019-11-17 13:57:55 +1000

    Initial commit

---
+++ kBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
