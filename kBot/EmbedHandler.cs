7fe5d9c67f72047f88c35239b6f8729738993657
Benjamin Hook <benjamin.hook@outlook.com>
2019-11-20 16:12:56 +1000

    Add echo command and embed functionality

---
+++ kBot/EmbedHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Discord;
+
+namespace kBot
+{
+    class EmbedHandler
+    {
+        static EmbedBuilder embed = new EmbedBuilder();
+
+        public static dynamic Normal(string message)
+        {
+            embed.WithDescription(message);
+            return embed.Build();
+        }
+
+        public static dynamic Good(string message)
+        {
+            string icon = "✅";
+            embed.WithDescription(String.Format("{0} {1}", icon, message));
+            embed.WithColor(new Color(119, 178, 85));
+            return embed.Build();
+        }
+
+        public static dynamic Bad(string message)
+        {
+            string icon = "⛔";
+            embed.WithDescription(String.Format("{0} {1}", icon, message));
+            embed.WithColor(new Color(190, 25, 49));
+            return embed.Build();
+        }
+
+        public static dynamic Info(string message)
+        {
+            string icon = "ℹ";
+            embed.WithDescription(String.Format("{0} {1}", icon, message));
+            embed.WithColor(new Color(59, 136, 195));
+            return embed.Build();
+        }
+
+        public static dynamic Alert(string message)
+        {
+            string icon = "⚠";
+            embed.WithDescription(String.Format("{0} {1}", icon, message));
+            embed.WithColor(new Color(255, 204, 76));
+            return embed.Build();
+        }
+    }
+}
---
+++ kBot/Modules/Misc.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace kBot.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            await Context.Channel.SendMessageAsync("", false, EmbedHandler.Normal(message));
+        }
+    }
+}
